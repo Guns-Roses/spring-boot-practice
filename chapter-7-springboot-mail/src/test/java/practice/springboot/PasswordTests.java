9d8e09339e20a8b07c5b55774fe2b3280a888da1
kevin.lv <kevin.lv@quectel.com>
2025-03-05 15:16:25 +0800

    修改发送邮件时测试方式的注释

--- chapter-7-springboot-mail/src/test/java/practice/springboot/PasswordTests.java
+++ chapter-7-springboot-mail/src/test/java/practice/springboot/PasswordTests.java
@@ -7,6 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 /**
  * @author kevin.lv
  * @date 2024/12/17 16:29
+ *  注意：测试时将yml中的配置修改为：test-connection: false
+ *  注意：测试时将yml中的配置修改为：test-connection: false
+ *  注意：测试时将yml中的配置修改为：test-connection: false
  */
 public class PasswordTests extends EmailApplicationTests {
 
@@ -22,7 +25,7 @@ public class PasswordTests extends EmailApplicationTests {
         String password = "your-email-password";
         // 加密后的密码(注意：配置上去的时候需要加 ENC(加密密码))
         String encryptPassword = encryptor.encrypt(password);
-        String decryptPassword = encryptor.decrypt("RNwjcgJaHNSy5HPRSm4bJMW6bPHVnC5iZVdrXYhhBJ+pTDNCz9gDKwYUjXtSk0OP");
+        String decryptPassword = encryptor.decrypt("tYdUSyMKBZWL3r5nlVYgW9AB4FaFdyaa15JAyyN7TPaOcPKZNJTNAzb7TExxUk0/w8hmfu8t5zII8n1vL4t9Zg==");
 
         System.out.println("password = " + password);
         System.out.println("encryptPassword = " + encryptPassword);
